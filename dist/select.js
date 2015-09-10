caadd0055e2aebecf670814fec609a864056e286
vladferix <phoerix@gmail.com>
2015-09-10 12:41:23 +0300

    Some optimization hack to speed up render and hiding

--- dist/select.js
+++ dist/select.js
@@ -209,7 +209,7 @@ uis.directive('uiSelectChoices',
         }
 
         choices.attr('ng-repeat', RepeatParser.getNgRepeatExpression($select.parserResult.itemName, '$select.items', $select.parserResult.trackByExp, groupByExp))
-            .attr('ng-if', '$select.open') //Prevent unnecessary watches when dropdown is closed
+            .attr('ng-show', '$select.open') //Prevent unnecessary watches when dropdown is closed
             .attr('ng-mouseenter', '$select.setActiveItem('+$select.parserResult.itemName +')')
             .attr('ng-click', '$select.select(' + $select.parserResult.itemName + ',false,$event)');
 
